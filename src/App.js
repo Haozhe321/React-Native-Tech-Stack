7912a44ed12e7fdbb2dad4a3852429512bfb60d7
Haozhe <hzthegreat@gmail.com>
2019-01-24 18:47:09 +0800

    Add reducers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux;';
+
+
+//store holds application state
+//provider translates the data in the store to something react can use
 
 const App = () => {
     return (
-        <View />
+        <Provider store={createStore}>
+            <View />
+        </Provider>
     );
 };
 
