0b90eb2d1eddcec5b92a9ca338a7dd235183da35
Haozhe <hzthegreat@gmail.com>
2019-01-27 14:44:04 +0800

    Add ItemList

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { FlatList } from 'react-native';
 import { connect } from 'react-redux';
+import ListItem from './ListItem';
 
 class LibraryList extends Component{
-    render() {
-        return;
+    renderItem(data) {
+        return <ListItem data={ data } />;
+    }
+
+    render()
+        return (
+            <FlatList
+                data={this.props.dataToShow}
+                renderItem={this.renderItem}
+                keyExtractor={(data) => data.id}
+            />
+        )
     }
 }
 
+//state is the argument
+/** mapStateToProps takes a global state object(application state),
+the thing that sits inside the redux store and map it and provide it as props
+to our library list
+**/
 const mapStateToProps = state => {
-    console.log(mapStateToProps);
+    //object returned here will show us as props in LibraryList
+    return { dataToShow: state.libraries}
 };
 
 export default connect(mapStateToProps)(LibraryList);
